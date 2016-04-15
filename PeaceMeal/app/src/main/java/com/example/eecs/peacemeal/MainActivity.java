40cdd33196fbcbcaca7abfac70878af0f734a4a1
Jennifer Hsu <jennifer.wen123@gmail.com>
2016-04-15 16:19:37 -0700

    initial commit

---
+++ PeaceMeal/app/src/main/java/com/example/eecs/peacemeal/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.eecs.peacemeal;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final Button facebookButton = (Button) findViewById(R.id.facebookLogin);
+        facebookButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Perform action on click
+                sendMessage(v);
+            }
+        });
+
+        final Button googleButton = (Button) findViewById(R.id.googleLogin);
+        googleButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Perform action on click
+            }
+        });
+
+    }
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, SelectionActivity.class);
+        startActivity(intent);
+    }
+
+}
---
+++ PeaceMeal/app/src/main/java/com/example/eecs/peacemeal/SelectionActivity.java
@@ -0,0 +1,33 @@
+package com.example.eecs.peacemeal;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class SelectionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_selection);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        final Button completeButton = (Button) findViewById(R.id.completeButton);
+        completeButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Perform action on click
+                sendMessage(v);
+            }
+        });
+    }
+
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, DishFeedActivity.class);
+        startActivity(intent);
+    }
+}
